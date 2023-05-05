a6e6a7c5e8e22f4fe1a624147e4d5a97593181be
Shohin Abdulkhamidov <abd.shohin@gmail.com>
2023-05-05 00:57:13 -0700

    update build.gradle

--- starbucks-cashier/src/main/java/com/example/starbuckscashier/StarbucksCashierApplication.java
+++ starbucks-cashier/src/main/java/com/example/starbuckscashier/StarbucksCashierApplication.java
@@ -2,14 +2,12 @@ package com.example.starbuckscashier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class SpringCashierApplication {
+public class StarbucksCashierApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCashierApplication.class, args);
+        SpringApplication.run(StarbucksCashierApplication.class, args);
     }
 
 
---
+++ starbucks-cashier/src/test/java/com/example/springstarbucksapi/StarbucksCashierApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.springstarbucksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootConfiguration
+@SpringBootTest
+class StarbucksCashierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
