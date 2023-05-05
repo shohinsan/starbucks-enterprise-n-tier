7ff389dc9c1cb32a091322a82a6773b194b0426f
Shohin Abdulkhamidov <abd.shohin@gmail.com>
2023-05-05 00:24:08 -0700

    update

--- starbucks-cashier/src/main/java/com/example/starbuckscashier/login/AuthController.java
+++ starbucks-cashier/src/main/java/com/example/starbuckscashier/login/AuthController.java
@@ -1,53 +1,59 @@
-package com.example.starbuckscashier.login;//package com.example.starbuckscashier.login;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//
-//import javax.validation.Valid;
-//import java.util.List;
-//
-//@Controller
-//public class AuthController {
-//    @Autowired
-//    UserService userService;
-//
-//    @RequestMapping(value = {"/login"}, method = RequestMethod.GET)
-//    public String login(Model model, @Valid User user, BindingResult bindingResult){
-//        return "auth/login";
-//    }
-//
-//    @RequestMapping(value = {"/register"}, method = RequestMethod.GET)
-//    public String register(Model model){
-//        model.addAttribute("user", new User());
-//        return "auth/login";
-//    }
-//
-//    @RequestMapping(value = {"/register"}, method = RequestMethod.POST)
-//    public String registerUser(Model model, @Valid User user, BindingResult bindingResult){
-//
-//        if(bindingResult.hasErrors()){
-//            System.out.println( "User registration failed!" ) ;
-//            model.addAttribute("successMessage", "User registration failed!");
-//            model.addAttribute("bindingResult", bindingResult);
-//            return "redirect:" + "register?regfailed=true";
-//        }
-//
-//        List<Object> userPresentObj = userService.isUserPresent(user);
-//        if((Boolean) userPresentObj.get(0)){
-//            System.out.println( "User already registered!" ) ;
-//            model.addAttribute("successMessage", userPresentObj.get(1));
-//            return "redirect:" + "register?userexists=true";
-//        }
-//
-//        user.setRole(Role.USER);
-//        userService.saveUser(user);
-//        model.addAttribute("successMessage", "User registered successfully!");
-//
-//        return "redirect:" + "login";
-//    }
-//}
-//
+package com.example.starbuckscashier.login;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = {"/login"}, method = RequestMethod.GET)
+    public String login(Model model, @Valid User user, BindingResult bindingResult){
+        return "auth/login";
+    }
+
+    @RequestMapping(value = {"/user/dashboard"}, method = RequestMethod.GET)
+    public String dashboard(Model model, @Valid User user, BindingResult bindingResult){
+        return "user/dashboard";
+    }
+
+    @RequestMapping(value = {"/register"}, method = RequestMethod.GET)
+    public String register(Model model){
+        model.addAttribute("user", new User());
+        return "auth/login";
+    }
+
+    @RequestMapping(value = {"/register"}, method = RequestMethod.POST)
+    public String registerUser(Model model, @Valid User user, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            System.out.println( "User registration failed!" ) ;
+            model.addAttribute("successMessage", "User registration failed!");
+            model.addAttribute("bindingResult", bindingResult);
+            return "redirect:/register?regfailed=true";
+        }
+
+        List<Object> userPresentObj = userService.isUserPresent(user);
+        if((Boolean) userPresentObj.get(0)){
+            System.out.println( "User already registered!" ) ;
+            model.addAttribute("successMessage", userPresentObj.get(1));
+            return "redirect:/register?userexists=true";
+        }
+
+        user.setRole(Role.USER);
+        userService.saveUser(user);
+        model.addAttribute("successMessage", "User registered successfully!");
+
+        return "redirect:/auth/login";
+    }
+}
+
--- starbucks-cashier/src/main/java/com/example/starbuckscashier/login/UserService.java
+++ starbucks-cashier/src/main/java/com/example/starbuckscashier/login/UserService.java
@@ -1,8 +1,8 @@
-package com.example.starbuckscashier.login;//package com.example.starbuckscashier.login;
-//
-//import java.util.List;
-//
-//public interface UserService {
-//    public void saveUser(User user);
-//    public List<Object> isUserPresent(User user);
-//}
+package com.example.starbuckscashier.login;
+
+import java.util.List;
+
+public interface UserService {
+    public void saveUser(User user);
+    public List<Object> isUserPresent(User user);
+}
